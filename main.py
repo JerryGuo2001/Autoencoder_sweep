a1121b77c71d691adfa26f99c3a980ddb2ab8cf9
JerryGuo2001 <jianleg@uci.edu>
2024-11-14 15:16:37 -0800

    updates

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch 
+from datetime import datetime
+import matplotlib.pyplot as plt
+from collections import defaultdict
+#from tqdm import tqdm # TODO: fix verbose training prints to logger or training bar
+import networkx as nx
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+# Now df_frame will have the mean PCA1 and PCA2 for each graph_id
+from sklearn.cluster import KMeans
+import seaborn as sns
+import sys
+
+# Get variables from command-line arguments
+partition = int(sys.argv[1])
+slurm_array_task_id = int(sys.argv[2])
+a = int(sys.argv[3])  # Capture 'a'
+
+print(f"Variable a: {a}")
+# Rest of the code
---
+++ mainbash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for((callback=1; callback<6; callback+=1)); do  
+    for((partition=0; partition<9; partition+=1)); do 
+        a=$((callback + partition))
+        sbatch file_startup.sub "$partition" "$callback" "$a"; 
+    done; 
+done;
