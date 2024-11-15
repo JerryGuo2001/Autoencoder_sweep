a3bca4c45baf093935a7d826ecb67dafc29698e9
JerryGuo2001 <jianleg@uci.edu>
2024-11-15 00:33:13 -0800

    im just crazy

--- MiniCouper/singlebash.sh
+++ MiniCouper/singlebash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for((callback=0; callback<2; callback+=1)); do
-    for((partition=0; partition<2; partition+=1)); do 
+for((callback=0; callback<10; callback+=1)); do
+    for((partition=0; partition<10; partition+=1)); do 
         sbatch file_startup.sub "$partition" "$callback"; 
     done; 
 done;
