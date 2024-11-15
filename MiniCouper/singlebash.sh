57cb384775456261000b43ca4abb27bd9cf6a52f
JerryGuo2001 <jianleg@uci.edu>
2024-11-14 23:29:44 -0800

    fixing bug

--- MiniCouper/singlebash.sh
+++ MiniCouper/singlebash.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+b=1
 for((callback=0; callback<1; callback+=1)); do 
     a=$(callback) 
     for((partition=0; partition<70; partition+=1)); do 
-        b=$(a*partition)
-        sbatch file_startup.sub "$partition" "$b" "$a"; 
+        sbatch file_startup.sub "$partition" "$b" "$a"
+        b=$((b + 1))
     done; 
 done;
