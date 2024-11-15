1a015d180615076a4daf46f39e722fc60b0a2356
JerryGuo2001 <jianleg@uci.edu>
2024-11-14 17:37:27 -0800

    updating bug

--- Startup/main.py
+++ Startup/main.py
@@ -3,16 +3,18 @@ import torch
 from datetime import datetime
 import matplotlib.pyplot as plt
 from collections import defaultdict
+from torch.utils.data import DataLoader, TensorDataset
+from torch import nn
+from torchsummary import summary # TODO: This should be a param / file write
 #from tqdm import tqdm # TODO: fix verbose training prints to logger or training bar
 import networkx as nx
 import pandas as pd
+from scipy.spatial.distance import cdist
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 # Now df_frame will have the mean PCA1 and PCA2 for each graph_id
 from sklearn.cluster import KMeans
 import seaborn as sns
-import sys
-from torchsummary import summary
 
 # Get variables from command-line arguments
 partition = int(sys.argv[1])
